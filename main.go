0989af4c4c916b36d7847d2e5cea420c438c7620
adyshkinss <adyshkinss@gmail.com>
2024-11-06 13:31:30 +0300

    del summm

--- main.go
+++ main.go
@@ -6,15 +6,14 @@ import (
 
 func main() {
 
-	fmt.Println(summ(2, 5))
 	fmt.Println(summm(2, 5, 8))
 
 }
 
-func summ(num1, num2 int) int {
-	return num1 + num2
-}
-
 func summm(num1, num2, num3 int) int {
 	return num1 + num2 + num3
+}
+
+func summmm(num1, num2, num3, num4 int) int {
+	return num1 + num2 + num3 + num4
 }
